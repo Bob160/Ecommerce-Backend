faa196f334f5d1649cb1c30156c1893447d5ebe7
Efio-esien Efiom <58000961+Bob160@users.noreply.github.com>
2023-02-15 15:27:40 +0100

    create endpoint for new category

--- src/main/java/com/efiom/ecommerce/services/CategoryService.java
+++ src/main/java/com/efiom/ecommerce/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.efiom.ecommerce.services;
 
+import com.efiom.ecommerce.models.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CategoryService {
+public interface CategoryService {
+
+    Category createCategory(String categoryName);
+    Category readCategory(String categoryName);
 
 
-    private CategoryService categoryService;
 }
