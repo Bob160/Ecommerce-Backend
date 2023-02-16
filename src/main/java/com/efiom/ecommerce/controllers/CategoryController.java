619dc3a26dc24224fb77ad6be475fceef4407e6e
Efio-esien Efiom <58000961+Bob160@users.noreply.github.com>
2023-02-16 13:01:36 +0100

    added read service in category

--- src/main/java/com/efiom/ecommerce/controllers/CategoryController.java
+++ src/main/java/com/efiom/ecommerce/controllers/CategoryController.java
@@ -1,6 +1,8 @@
 package com.efiom.ecommerce.controllers;
 
 import com.efiom.ecommerce.config.BaseResponse;
+import com.efiom.ecommerce.enums.ResponseCodeEnum;
+import com.efiom.ecommerce.models.Category;
 import com.efiom.ecommerce.pojos.requests.CategoryDto;
 import com.efiom.ecommerce.pojos.responses.CreateCategoryResponse;
 import com.efiom.ecommerce.services.CategoryService;
@@ -8,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -23,11 +22,19 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
+    private final Category category;
+
 
     @PostMapping("/create-category")
     public ResponseEntity<CreateCategoryResponse> createCategory(@Valid @RequestBody CategoryDto categoryDto) {
         CreateCategoryResponse categoryResponse = (CreateCategoryResponse) categoryService.createCategory(categoryDto);
         return new ResponseEntity<>(categoryResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/category")
+    public ResponseEntity<Category> readCategory (@RequestBody CategoryDto categoryDto) {
+        Category categoryResponse = categoryService.readCategory(categoryDto);
+        return new ResponseEntity<>(category, HttpStatus.OK);
     }
 
 }
