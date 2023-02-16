a1f41c48ac8b77ff7a7fc2c49f8bbe65907e880a
Efio-esien Efiom <58000961+Bob160@users.noreply.github.com>
2023-02-16 03:43:50 +0100

    modified category endpoint

---
+++ src/main/java/com/efiom/ecommerce/pojos/responses/CreateCategoryResponse.java
@@ -0,0 +1,14 @@
+package com.efiom.ecommerce.pojos.responses;
+
+import com.efiom.ecommerce.config.BaseResponse;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Builder
+@ToString
+@Data
+public class CreateCategoryResponse extends BaseResponse {
+
+    private String categoryName;
+}
--- src/main/java/com/efiom/ecommerce/services/serviceImpl/CategoryServiceImpl.java
+++ src/main/java/com/efiom/ecommerce/services/serviceImpl/CategoryServiceImpl.java
@@ -1,27 +1,44 @@
 package com.efiom.ecommerce.services.serviceImpl;
 
+import com.efiom.ecommerce.enums.ResponseCodeEnum;
 import com.efiom.ecommerce.models.Category;
+import com.efiom.ecommerce.pojos.requests.CategoryDto;
+import com.efiom.ecommerce.pojos.responses.CreateCategoryResponse;
 import com.efiom.ecommerce.repositories.CategoryRepository;
 import com.efiom.ecommerce.services.CategoryService;
+import com.efiom.ecommerce.util.ResponseCodeUtil;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
-
-    @Autowired
-    public CategoryRepository categoryRepository;
-    public CategoryService categoryService;
+    private final CategoryRepository categoryRepository;
+    //public final CategoryService categoryService;
+    private final ResponseCodeUtil responseCodeUtil = new ResponseCodeUtil();
 //!categoryRepository.findByCategoryName(categoryName).isPresent()
     @Override
-    public Category createCategory(String categoryName) {
-        if (categoryRepository.findByCategoryName(categoryName) == null) {
-            return categoryService.createCategory(categoryName);
+    public CreateCategoryResponse createCategory(CategoryDto categoryDto) {
+        CreateCategoryResponse categoryResponse = CreateCategoryResponse.builder()
+                .categoryName(categoryDto.getCategoryName())
+                .build();
+        if (categoryRepository.findByCategoryName(categoryDto.getCategoryName()) == null) {
+            Category category = new Category();
+            BeanUtils.copyProperties(categoryDto, category);
+            categoryRepository.save(category);
+            return responseCodeUtil.updateResponseData(categoryResponse, ResponseCodeEnum.SUCCESS, "New ${category} created");
+            //return categoryService.createCategory(categoryDto);
             //categoryRepository.save(categoryName);
+
         }
-        return new ApiResponse(false,"Duplicate Category");
+        //return new ApiResponse(false,"Duplicate Category");
+
+        return responseCodeUtil.updateResponseData(categoryResponse, ResponseCodeEnum.ERROR, "Category already exists");
     }
 
     @Override
